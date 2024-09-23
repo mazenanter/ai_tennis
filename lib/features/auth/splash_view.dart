c1f1476f375bc005043df303d15135dad0d0faf3
MaZen <MazenAnter>
2024-09-23 18:53:50 +0300

    edit splash screen

--- lib/features/auth/splash_view.dart
+++ lib/features/auth/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:ai_tennis/core/app_routes/app_routes.dart';
+import 'package:ai_tennis/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,43 +10,54 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<AlignmentGeometry> _alignAnimation;
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late AnimationController _rotationController;
+  late Animation<double> _fadeAnimation;
   late Animation<double> _rotationAnimation;
+
   @override
   void initState() {
     super.initState();
-    initAnimation();
+    initAnimations();
     navigateToLoginView();
   }
 
-  void initAnimation() {
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 800),
+  void initAnimations() {
+    _fadeController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
       vsync: this,
-    )..repeat(reverse: true);
-    _alignAnimation = Tween<AlignmentGeometry>(
-      begin: Alignment.centerLeft,
-      end: Alignment.centerRight,
-    ).animate(
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
       CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOutCubic,
+        parent: _fadeController,
+        curve: Curves.easeIn,
       ),
     );
-    _rotationAnimation = Tween<double>(begin: 0, end: 2).animate(
+
+    Future.delayed(const Duration(milliseconds: 500), () {
+      _fadeController.forward();
+    });
+
+    _rotationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _rotationAnimation = Tween<double>(begin: 0, end: 1).animate(
       CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOutCubic,
+        parent: _rotationController,
+        curve: Curves.linear,
       ),
     );
+
+    _rotationController.repeat();
   }
 
   void navigateToLoginView() {
     Future.delayed(
-      const Duration(seconds: 4),
+      const Duration(seconds: 3),
       () {
         if (mounted) {
           context.go(AppRoutes.kLoginOrSignUpView);
@@ -57,37 +69,45 @@ class _SplashViewState extends State<SplashView>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.center,
+      backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.network(
-                fit: BoxFit.fill,
-                height: MediaQuery.sizeOf(context).height,
-                'https://us.123rf.com/450wm/christovstudio/christovstudio2102/christovstudio210201270/164418471-tennis-men-smash-line-pop-art-potrait-logo-colorful-design-with-dark-background-abstract-vector.jpg',
-              ),
-              AlignTransition(
-                alignment: _alignAnimation,
-                child: RotationTransition(
-                  turns: _rotationAnimation,
-                  child: Image.asset(
-                    height: 80,
-                    width: 80,
-                    'assets/images/ben-hershey-VEW78A1YZ6I-unsplash 1.png',
+              FadeTransition(
+                opacity: _fadeAnimation,
+                child: const Text(
+                  'Tennis AI',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white,
                   ),
+                ),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              RotationTransition(
+                turns: _rotationAnimation,
+                child: Image.asset(
+                  height: 80,
+                  width: 80,
+                  'assets/images/ben-hershey-VEW78A1YZ6I-unsplash 1.png',
                 ),
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _fadeController.dispose();
+    _rotationController.dispose();
     super.dispose();
   }
 }
