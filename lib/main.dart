a5d90ff528a4991e2ce81ef5bb435fbb4882bde7
MaZen <MazenAnter>
2024-09-25 16:17:19 +0300

    add doc file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:ai_tennis/core/app_routes/app_routes.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) {
-        return const MyApp();
-      }));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,8 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       routerConfig: AppRoutes.router,
       debugShowCheckedModeBanner: false,
       title: 'Ai tennis',
